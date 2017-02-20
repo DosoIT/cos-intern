ece8c90a983964e1f15c5a30a5810e2d0a01cfd6
Jeremylinkk <Siriwut-77@hotmail.com>
2017-02-20 16:44:32 +0700

    upupup

--- cos/routes.js
+++ cos/routes.js
@@ -11,8 +11,9 @@ module.exports = function (app, passport, urlencodedParser, jsonParser, session,
     var storage = new multer.diskStorage({
         destination: './uploads/',
             filename: function (req, file, cb) {
+            filenames = "";
             filenames = "Avatar" + (Math.floor((Math.random() * 1000) + 1)) + path.extname(file.originalname);
-            cb(null, filenames)
+            cb(null, filenames);
             }
         }
     );
@@ -21,7 +22,6 @@ module.exports = function (app, passport, urlencodedParser, jsonParser, session,
         destination: './uploads/files/',
             filename: function (req, file, cb) {
             filenames = "file_" + (Math.floor((Math.random() * 1000) + 1)) + path.extname(file.originalname);
-            filenameArray.push(filenames);
             cb(null, filenames)
             }
         }
@@ -251,16 +251,15 @@ module.exports = function (app, passport, urlencodedParser, jsonParser, session,
     app.post( '/drangFiles',der.any(),isLoggedIn,function( req, res, next){
         // console.log(req.files);
         console.log(filenames);
-        // filenameArray=[];
-        // if(filenames != ""){
-        //     var getFile=messages.messageInsert(req.body.user_id,'',req.body.userReply,filenames);
-        //     if(getFile){
-        //         console.log("upload files complete..");
-        //     }
-        //     else{
-        //         console.log("Files upload Error!!");
-        //     }
-        // }
+        if(filenames != ""){
+            var getFile=messages.messageInsert(req.body.user_id,'',req.body.userReply,filenames);
+            if(getFile){
+                console.log("upload files complete..");
+            }
+            else{
+                console.log("Files upload Error!!");
+            }
+        }
         return res.status( 200 ).send(req.files);
     });
 
